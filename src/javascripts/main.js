353eaf4a3cdda38617960c58f172465aa9f2144f
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-03-07 12:26:42 -0600

    added sorting buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,7 @@
 import studentData from './helpers/data/studentData';
 import navbar from './components/navbar/navbar';
 import studentList from './components/studentList/studentList';
+import houseButtonGroup from './components/houseButtonGroup/houseButtonGroup';
 import '../styles/main.scss';
 
 
@@ -8,6 +9,7 @@ const init = () => {
   const allStudents = studentData.getStudents();
   navbar.loadNavbar();
   studentList.createStudentList(allStudents);
+  houseButtonGroup.createHouseButtonGroup();
 };
 
 init();
